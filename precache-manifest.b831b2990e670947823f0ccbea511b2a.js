e89d7092bb7274877bc7db2ba931c3c808698b50
filay <return.jk@gmail.com>
2020-09-23 16:29:09 +0900

    Updates

---
+++ precache-manifest.b831b2990e670947823f0ccbea511b2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7743da5cb98f7f4f3910decb3a2495d",
+    "url": "/study_filay/index.html"
+  },
+  {
+    "revision": "f9565c1a3c86a48c8496",
+    "url": "/study_filay/static/css/main.3a2dfd48.chunk.css"
+  },
+  {
+    "revision": "996a22611ec8c275b65f",
+    "url": "/study_filay/static/js/2.1c97c4ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/study_filay/static/js/2.1c97c4ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9565c1a3c86a48c8496",
+    "url": "/study_filay/static/js/main.71cc7c0c.chunk.js"
+  },
+  {
+    "revision": "d26b58b92e7d2923651a",
+    "url": "/study_filay/static/js/runtime-main.9250a44d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/study_filay/index.html/precache-manifest.7dbd342473bfb527c5a6c7481fe889ed.js"
+  "/study_filay/precache-manifest.b831b2990e670947823f0ccbea511b2a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/study_filay/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/study_filay/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
